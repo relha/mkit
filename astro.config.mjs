a2fa7b7c0a2638300cc64c72fc386836dc0be9bc
Administrator <contact@make-it-france.fr>
2025-03-17 20:42:01 +0100

    correction des urls de la navbar ainsi que les couleurs du logo MKIT

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://ai-solutions.com',
+  site: 'https://mkit.fr',
   base: '/',
   server: {
     port: 4321,
